6441d1a4bfafb4614e063d713f7968ef9c488417
abdullahimran800 <abdullahimrandeveloper789@gmail.com>
2025-08-15 10:58:05 +0000

    variabels

---
+++ O1-basics/O1-variables.js
@@ -0,0 +1,13 @@
+const accountid= 144553
+let accountemail= "abdul@gmail.com"
+var accountpassword= 12345
+accountcity= "karachi"
+
+ accountemail= "abdullah@gmail.com"
+ accountpassword= "54321"
+ accountcity="lahore"
+
+
+console.log(accountid);
+console.table([accountid, accountemail, accountpassword, accountcity])
+
